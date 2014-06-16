c1ba5faa4d3371b89403b67120280df474703114
Tom Fenton <tom@ncs.co.nz>
2014-06-16 22:01:33 +1200

    Add requires to errors.rb.

--- lib/inforouter/errors.rb
+++ lib/inforouter/errors.rb
@@ -1,4 +1,6 @@
 require 'inforouter/errors/inforouter_error'
+require 'inforouter/errors/api_error'
 require 'inforouter/errors/missing_config_error'
 require 'inforouter/errors/missing_config_option_error'
 require 'inforouter/errors/soap_error'
+require 'inforouter/errors/unexpected_soap_response_error.rb'

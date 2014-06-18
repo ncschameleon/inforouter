a717f5b6392e63838fd5b2914a47bf002c061e3d
Tom Fenton <tom@ncs.co.nz>
2014-06-19 11:17:34 +1200

    Add lookup by user name.

--- lib/inforouter/users.rb
+++ lib/inforouter/users.rb
@@ -5,9 +5,18 @@ module Inforouter #:nodoc:
       def all
         @users ||= begin
           response = Inforouter.client.request :get_all_users
-          users = Inforouter::Responses::GetAllUsers.parse response
-          Hash[users.map { |user| [user.user_id, user] }]
+          users = Inforouter::Responses::Users.parse response
+          Hash[users.map { |user| [user.user_name, user] }]
         end
+      end
+
+      # Lookup a user by user name.
+      #
+      # @param user_name [String]
+      #
+      # @return [Inforouter::User]
+      def [](user_name)
+        all[user_name]
       end
     end
   end

2502a4632ff28fb4260a20a837d34ab4de30e540
Tom Fenton <tom@ncs.co.nz>
2014-06-20 14:04:07 +1200

    Add folder methods, use params methods.

--- lib/inforouter/folder.rb
+++ lib/inforouter/folder.rb
@@ -18,11 +18,55 @@ module Inforouter #:nodoc:
     # Folder domain ID.
     attr_accessor :domain_id
 
+    def description
+      @description ||= begin
+        response = Inforouter.client.request :get_folder, folder_params
+        folder = Inforouter::Responses::Folder.parse response
+        @description = folder.description
+      end
+    end
+
+    def create
+      response = Inforouter.client.request :create_folder, path_params
+      result = Inforouter::Responses::CreateFolder.parse response
+      result[:@success] == 'true'
+    end
+
     def exists?
-      message = { :path => @path }
-      response = Inforouter.client.request :folder_exists, message
+      response = Inforouter.client.request :folder_exists, path_params
       result = Inforouter::Responses::FolderExists.parse response
       result[:@success] == 'true'
+    end
+
+    def update_properties
+      message = {
+        :path => @path,
+        :new_folder_name => @name,
+        :description => @description
+      }
+      response = Inforouter.client.request :update_folder_properties, message
+      result = Inforouter::Responses::UpdateFolderProperties.parse response
+      result[:@success] == 'true'
+    end
+
+    private
+
+    # @return [Hash]
+    def path_params
+      {
+        :path => @path
+      }
+    end
+
+    # @return [Hash]
+    def folder_params
+      {
+        :path => @path,
+        :with_rules => 0,
+        'withPropertySets' => 0,
+        'withSecurity' => 0,
+        'withOwner' => 0
+      }
     end
   end
 end
